442677779d2727f02876268f3c99b3546ec2fe88
Jarmo Lammi <jjlammi@yahoo.com>
2016-03-04 23:48:12 +0200

    input action handler

--- app/components/todo-input.js
+++ app/components/todo-input.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({    actions: {
-     submitTodo(newTitle) {
+export default Ember.Component.extend({
+        actions: {
+          submitTodo(newTitle) {
             if (newTitle) {
                 this.sendAction('action', newTitle);
             }
