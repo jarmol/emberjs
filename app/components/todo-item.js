bf099c737e875195e55580537bd8f24e77cf08a6
Jarmo Lammi <jjlammi@yahoo.com>
2016-03-05 15:51:08 +0200

    edit ok

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -5,9 +5,21 @@ export default Ember.Component.extend({
     classNameBindings: ['editing'],
     editing: false,
     actions: {
-        editTodo() {
-            this.toggleProperty('editing');
+    editTodo() {this.toggleProperty('editing');},
+    submitTodo() {
+        let todo = this.get('todo');
+        if (todo.get('title') === "") {
+            this.sendAction('deleteTodo', todo);
+        } else {
+            this.sendAction('updateTodo', this.get('todo'));
         }
+        this.set('editing', false);
+      },
+      deleteTodo() {
+        let todo = this.get('todo');
+        this.sendAction('deleteTodo', todo);
+      }
+
     }
-    
+        
 });
